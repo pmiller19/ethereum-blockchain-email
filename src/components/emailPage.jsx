a38cfd88c54426d3a1c644fb8e56f9292f9e9755
Parker Miller <54636061+pmiller19@users.noreply.github.com>
2022-12-03 13:35:35 -0600

    add ability to copy the address in email page

--- src/components/emailPage.jsx
+++ src/components/emailPage.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const EmailPage = ({ from, subject, body, updateSetOpenEmail }) => {
   const navigate = useNavigate();
+  const [copied, setCopied] = useState(false);
 
   return (
     <div>
@@ -18,7 +20,21 @@ const EmailPage = ({ from, subject, body, updateSetOpenEmail }) => {
       <div className='max-w-xl ml-auto mr-auto'>
         <div className='flex'>
           <p className='text-lg mb-2'>{"From: " + from}</p>
-          <button className='text-xl ml-auto'>↩</button>
+          <CopyToClipboard text={from} onCopy={() => setCopied(true)}>
+            <button
+              className={`mb-2 ml-1 ${
+                !copied ? " text-blue-500 hover:underline" : " cursor-auto"
+              }`}
+            >
+              {copied ? "✓" : "Copy"}
+            </button>
+          </CopyToClipboard>
+          <button
+            onClick={() => navigate("compose")}
+            className='text-xl ml-auto'
+          >
+            ↩
+          </button>
         </div>
         {body.split("\n").map((i, key) => {
           return <div key={key}>{i}</div>;
